b0fadf4fb2a0c3163abfd1493590668b40ce1f0f
serhiiKhromov <khromovserhii@gmail.com>
2018-05-10 21:02:09 +0300

    Исправлено меню, добавлен ROBOTO

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -4,17 +4,17 @@ import './Menu.scss'
 
 const Navbar = () => (
   <ul>
-      <li><NavLink exact to='/' activeClassName='active-menu-link' >Home</NavLink></li>
-      <span>●</span>
-      <li><NavLink to='/blog' activeClassName='active-menu-link' >Blog</NavLink></li>
-      <span>●</span>
-      <li><NavLink to='/products' activeClassName='active-menu-link' >Products</NavLink></li>
-      <span>●</span>
-      <li><NavLink to='/podcasts' activeClassName='active-menu-link' >Podcasts</NavLink></li>
-      <span>●</span>
-      <li><NavLink to='/my-story' activeClassName='active-menu-link' >My Story</NavLink></li>
-      <span>●</span>
-      <li><NavLink to='/contacts' activeClassName='active-menu-link' >Contact</NavLink></li>
+      <li><NavLink exact to='/' activeClassName='active-menu-link' >HOME</NavLink></li>
+      <span>•</span>
+      <li><NavLink to='/blog' activeClassName='active-menu-link' >BLOG</NavLink></li>
+      <span>•</span>
+      <li><NavLink to='/products' activeClassName='active-menu-link' >PRODUCTS</NavLink></li>
+      <span>•</span>
+      <li><NavLink to='/podcasts' activeClassName='active-menu-link' >PODCASTS</NavLink></li>
+      <span>•</span>
+      <li><NavLink to='/my-story' activeClassName='active-menu-link' >MY STORY</NavLink></li>
+      <span>•</span>
+      <li><NavLink to='/contacts' activeClassName='active-menu-link' >CONTACT</NavLink></li>
   </ul>
 )
 
