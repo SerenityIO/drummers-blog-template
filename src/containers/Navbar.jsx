5dfa3f9a4e3aa6a81e00b651608bec42a67b0c08
serhiiKhromov <khromovserhii@gmail.com>
2018-05-11 19:55:38 +0300

    Переделал меню через map()

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -2,19 +2,36 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Menu.scss'
 
+const arr = [
+  {
+    text: 'HOME',
+    to: '/'
+  }, 
+  {
+    text: 'BLOG',
+    to: '/blog'
+  }, 
+  {
+    text: 'PRODUCTS',
+    to: '/products'
+  }, 
+  {
+    text: 'PODCASTS',
+    to: '/podcasts'
+  }, 
+  {
+    text: 'MY STORY',
+    to: '/my-story'
+  }, 
+  {
+    text: 'CONTACT',
+    to: '/contacts'
+  }
+];
+
 const Navbar = () => (
   <ul>
-      <li><NavLink exact to='/' activeClassName='active-menu-link' >HOME</NavLink></li>
-      <span>•</span>
-      <li><NavLink to='/blog' activeClassName='active-menu-link' >BLOG</NavLink></li>
-      <span>•</span>
-      <li><NavLink to='/products' activeClassName='active-menu-link' >PRODUCTS</NavLink></li>
-      <span>•</span>
-      <li><NavLink to='/podcasts' activeClassName='active-menu-link' >PODCASTS</NavLink></li>
-      <span>•</span>
-      <li><NavLink to='/my-story' activeClassName='active-menu-link' >MY STORY</NavLink></li>
-      <span>•</span>
-      <li><NavLink to='/contacts' activeClassName='active-menu-link' >CONTACT</NavLink></li>
+      {arr.map((item, index) => <li key={index}><NavLink exact to={item.to} activeClassName='active-menu-link' >{item.text}</NavLink></li>)}
   </ul>
 )
 
